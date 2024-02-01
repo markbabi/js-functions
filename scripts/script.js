9a5404f084f88ae2e4e854a3dfdc1026fc19b735
Mark Babirad <mbabirad@brainstation.io>
2024-02-01 11:39:18 -0800

    create functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,39 @@
 console.log("Hello, welcome to FUN-ctions!!");
 
-let dogAge = 5;
-let dogName = "Leo";
-
 function functionName() {
   //add some code
 }
+
+let dogAge = 5;
+let dogName = "Leo";
+
+function formatDogAge(name, age) {
+  let formattedAge = "Doggie " + name + " is " + age * 7 + " in human years";
+  return formattedAge;
+}
+
+let formattedDogAge = formatDogAge("Bella", 10);
+console.log(formattedDogAge);
+
+let formattedLeo = formatDogAge(dogName, dogAge);
+console.log(formattedLeo);
+
+console.log(formatDogAge("Poochi", 3));
+console.log(formatDogAge("Bob", 2));
+console.log(formatDogAge("Buddy", 5));
+console.log(formatDogAge("Java", 100));
+
+function sayHello(name) {
+  return "Hello " + name;
+}
+
+let hello = sayHello("Jim");
+console.log(hello);
+
+function addNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(addNumbers("mark", 300));
+
+alert("this is an alert");
